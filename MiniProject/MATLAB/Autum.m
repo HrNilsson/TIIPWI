d09599e31056cabf8e74f195dae76e44eff71cfa
KapSteR <hr.kasper.nielsen@gmail.com>
2014-12-19 00:21:57 +0100

    Lots of NICE Figures

---
+++ MiniProject/MATLAB/Autum.m
@@ -0,0 +1,39 @@
+clear; clc;
+
+InputDataAutum;
+
+figure(1)
+clf
+% plot(Time,CO2)
+% hold on
+% plot(Time, Temp)
+% plot(Time, Wind)
+
+[hAx,hLine1,hLine2] = plotyy(Time,CO2,Time,Temp)
+hAx(1).XTickLabelRotation = -30
+hold on
+
+title('Data for October 15th')
+xlabel('Time')
+
+ylabel(hAx(1),'gram CO_2 / kWh') % left y-axis
+ylabel(hAx(2),'Wind [m/s] | Temperature [C]') % right y-axis
+
+m_CO2 = mean(CO2)
+plot(Time,ones(size(CO2))*m_CO2, '--k')   % Dayly mean
+plot(Time,ones(size(CO2))*340, '-.b')     % Yearly mean
+
+
+grid minor
+legend(...
+    'CO_2 emission [gram CO_2/kWH]',...
+    'Daily mean CO_2', ...
+    'Yearly mean CO_2',...
+    'Temperature [C]',...
+    'Location', 'northwest')
+
+scale = 0.1;
+pos = get(gca, 'Position');
+pos(2) = pos(2)+scale*pos(4);
+pos(4) = (1-scale)*pos(4);
+set(gca, 'Position', pos)
---
+++ MiniProject/MATLAB/InputData.m
@@ -0,0 +1,3 @@
+clear; clc;
+
+dlmread('20140720_onlinedata.txt',';',22,0,[0 0 (310-23) 20])
